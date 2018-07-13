1eae559019ba1e9fd160c5166af8a2debce65da7
jiawei-sanxing <892369583@qq.com>
2018-07-13 11:16:24 +0800

    跨域修改

--- cloud_parent/rest_webapp/src/main/java/com/ffxl/api/interceptor/PostHeaderInterceptor.java
+++ cloud_parent/rest_webapp/src/main/java/com/ffxl/api/interceptor/PostHeaderInterceptor.java
@@ -35,59 +35,72 @@ public class PostHeaderInterceptor extends HandlerInterceptorAdapter{
         
         @Override
         public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-            logger.info("开始执行Token拦截器------------------------");
-            HttpHeader header = new HttpHeader();
-            if (request.getServletPath().equals("/" + jwtProperties.getAuthPath())) {
-                // TODO 可在此处验证头参数
-                HttpHeader.set(header);
-                return true;
-            }
-            final String requestHeader = request.getHeader(jwtProperties.getHeader());
-            String sign = request.getHeader(HttpHeader.SIGN); //加密串，可自行规定是否使用
-            String authToken = null;
-            if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
-                authToken = requestHeader.substring(7);
-                //验证token是否过期,包含了验证jwt是否正确
-                try {
-                    boolean flag = jwtTokenUtil.isTokenExpired(authToken);
-                    if (flag) {
-                        RenderUtil.renderJson(response, new JsonResult(Message.M3007));
+            if (request.getMethod().equals("OPTIONS")) {
+              //首次请求，放行
+              logger.info("复杂跨域的试探OPTIONS请求放行》》》》》》》》》》");
+              return true;
+            }else{
+                logger.info("开始执行Token拦截器------------------------");
+                HttpHeader header = new HttpHeader();
+                if (request.getServletPath().equals("/" + jwtProperties.getAuthPath())) {
+                    // TODO 可在此处验证头参数
+                    HttpHeader.set(header);
+                    logger.info("未被拦截-----------1111》》》》》》》》》》"+request.getServletPath());
+                    return true;
+                }   
+                final String requestHeader = request.getHeader(jwtProperties.getHeader());
+                String sign = request.getHeader(HttpHeader.SIGN); //加密串，可自行规定是否使用
+                String authToken = null;
+                logger.info("输出-------------------"+requestHeader);
+                if (requestHeader != null && requestHeader.startsWith("Bearer ")) {
+                    authToken = requestHeader.substring(7);
+                    //验证token是否过期,包含了验证jwt是否正确
+                    try {
+                        boolean flag = jwtTokenUtil.isTokenExpired(authToken);
+                        if (flag) {
+                            RenderUtil.renderJson(response, new JsonResult(Message.M3007));
+                            logger.info("被拦截-----------1111-------------"+request.getServletPath());
+                            return false;
+                        }
+                        String loginName = jwtTokenUtil.getUsernameFromToken(authToken);//获取用户id
+                        Date date = jwtTokenUtil.getIssuedAtDateFromToken(authToken);//获取jwt发布时间
+                        String md5key = jwtTokenUtil.getMd5KeyFromToken(authToken);//获取jwt发布时间
+                        //解析token中的其他头参数
+                        String ip = jwtTokenUtil.getPrivateClaimFromToken(authToken, HttpHeader.IP);
+                        //TODO 在此获取头参数
+                        
+                        //封装登陆用户
+                        header.setUserId(loginName);
+                        header.setMd5key(md5key);
+                        header.setSign(sign);
+                        HttpHeader.set(header);
+                       
+                        logger.info("请求IP:"+ ip ); 
+                        logger.info("请求由"+loginName+"在【"+DateUtil.formatStandardDatetime(date)+"】发起请求");        
+                        
+                    } catch (JwtException e) {
+                        e.printStackTrace();
+                        //有异常就是token解析失败
+                        RenderUtil.renderJson(response,new JsonResult(Message.M3008));
+                        logger.info("被拦截--------------2222----------"+request.getServletPath());
                         return false;
                     }
-                    String loginName = jwtTokenUtil.getUsernameFromToken(authToken);//获取用户id
-                    Date date = jwtTokenUtil.getIssuedAtDateFromToken(authToken);//获取jwt发布时间
-                    String md5key = jwtTokenUtil.getMd5KeyFromToken(authToken);//获取jwt发布时间
-                    //解析token中的其他头参数
-                    String ip = jwtTokenUtil.getPrivateClaimFromToken(authToken, HttpHeader.IP);
-                    //TODO 在此获取头参数
-                    
-                    //封装登陆用户
-                    header.setUserId(loginName);
-                    header.setMd5key(md5key);
-                    header.setSign(sign);
-                    HttpHeader.set(header);
-                   
-                    logger.info("请求IP:"+ ip ); 
-                    logger.info("请求由"+loginName+"在【"+DateUtil.formatStandardDatetime(date)+"】发起请求");        
-                    
-                } catch (JwtException e) {
-                    //有异常就是token解析失败
-                    RenderUtil.renderJson(response,new JsonResult(Message.M3008));
+                    logger.info("未被拦截-----------2222》》》》》》》》》》"+request.getServletPath());
+                    return true;
+                } else {
+                    //无需鉴权
+                    if(JwtProperties.getNoAuth().contains(request.getServletPath())){
+                        logger.info("未被拦截-----------333333》》》》》》》》》》"+request.getServletPath());
+                        return true;
+                    }else{
+                       logger.info("-------------------被拦截"+request.getServletPath());
+                    }
+                    //header没有带Bearer字段
+                    RenderUtil.renderJson(response,new JsonResult(Message.M3010));
+                    logger.info("被拦截------------33333------------"+request.getServletPath());
                     return false;
                 }
-                return true;
-            } else {
-                //无需鉴权
-                if(JwtProperties.getNoAuth().contains(request.getServletPath())){
-                    return true;
-                }else{
-                   logger.info("-------------------被拦截"+request.getServletPath());
-                }
-                //header没有带Bearer字段
-                RenderUtil.renderJson(response,new JsonResult(Message.M3010));
-                return false;
             }
-            
     }
 
     /**
