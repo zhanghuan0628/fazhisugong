82f7153dc2d08a77c3f95b149e64e091a181ca45
PC0011\feifan <long.zhao@feifanxinli.com>
2018-07-17 18:38:11 +0800

    提交

--- cloud_parent/admin_webapp/src/main/webapp/static/modular/fzsg/law_hall/law_hall_info.js
+++ cloud_parent/admin_webapp/src/main/webapp/static/modular/fzsg/law_hall/law_hall_info.js
@@ -28,7 +28,7 @@ var editor = new wangEditor('editor');
  editor.config.pasteFilter = false;
 
  // 上传图片（举例）
- /*editor.config.uploadImgUrl = '${base}/yy_wechat/common/img_upload';*/
+ editor.config.uploadImgUrl = Feng.ctxPath + '/common/img_upload';
 
  // 配置自定义参数（举例）
  editor.config.uploadParams = {
